084def1579d238a6e28836ebccb8155830ee7730
okotoazachristian <okotoazachristian@gmail.com>
2023-11-29 04:39:56 -0800

    conditional statment to check if last digit is greater 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 last_Digit = abs(number) % 10
-if last_Digit < 6 or number < 0:
+if last_Digit == 0:
+    print(f"Last digit of {number} is {last_Digit} and is 0")
+elif last_Digit < 6 or number < 0:
     if number < 0:
         last_Digit = -last_Digit
         print(f"Last digit of {number} is {last_Digit} and is less than 6 and not 0")
@@ -10,5 +12,3 @@ if last_Digit < 6 or number < 0:
         print(f"Last digit of {number} is {last_Digit} and is less than 6 and not 0")
 elif last_Digit > 5:
     print(f"Last digit of {number} is {last_Digit} and is greater than 5")
-else:
-    print(f"Last digit of {number} is {last_Digit} and is 0")
