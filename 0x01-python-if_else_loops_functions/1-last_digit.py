7bf032acd58125505c262d307ce39bdac6db2502
okotoazachristian <okotoazachristian@gmail.com>
2023-11-29 05:41:04 -0800

    variable comparisim

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_Digit = abs(number) % 10
-if last_Digit == 0:
-    print(f"Last digit of {number} is {last_Digit} and is 0")
-elif last_Digit < 6 or number < 0:
+Digit = abs(number) % 10
+if Digit == 0:
+    print(f"Last digit of {number} is {Digit} and is 0")
+elif Digit < 6 or number < 0:
     if number < 0:
-        last_Digit = -last_Digit
-        print(f"Last digit of {number} is {last_Digit} and is less than 6 and not 0")
+        Digit = -Digit
+        print(f"Last digit of {number} is {Digit} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {last_Digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {Digit} and is less than 6 and not 0")
 elif last_Digit > 5:
-    print(f"Last digit of {number} is {last_Digit} and is greater than 5")
+    print(f"Last digit of {number} is {Digit} and is greater than 5")
