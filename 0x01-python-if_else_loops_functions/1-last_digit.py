9cc45f3860a5715b680684948f6de53f367e2c0b
okotoazachristian <okotoazachristian@gmail.com>
2023-11-29 04:16:54 -0800

    completing a code to text for condition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 last_Digit = abs(number) % 10
-if last_Digit < 6 and last_Digit != 0:
+if last_Digit < 6 or number < 0:
     if number < 0:
         last_Digit = -last_Digit
         print(f"Last digit of {number} is {last_Digit} and is less than 6 and not 0")
