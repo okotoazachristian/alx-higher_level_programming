90ad8ff73a329b424ea13f9c60722532fd1698be
okotoazachristian <okotoazachristian@gmail.com>
2023-12-05 11:31:45 -0800

    error fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return("".join(listofchars))
+    dict = {ord(char): None for char in "cC"}
+    new_string = my_string.translate(dict)
+    return new_string
