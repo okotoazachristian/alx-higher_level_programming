ea6106a50da9a08b4fc5b94af4028d2b378ffe93
okotoazachristian <okotoazachristian@gmail.com>
2023-12-05 12:04:38 -0800

    bug fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        sub_len = len(matrix[i])
-        for j in range(sub_len):
-            if j != sub_len - 1:
-                end_char = ' '
-            else:
-                end_char = ''
-            print("{:d}".format(matrix[i][j]), end=end_char)
-        print("")
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c not in 'cC':
+            new_string += c
+    return new_string
