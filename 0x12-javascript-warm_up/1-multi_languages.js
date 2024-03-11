c607ff474891006a3751e80a31d9bb8038366b73
okotoazachristian <okotoazachristian@gmail.com>
2024-03-11 14:43:06 -0700

    c is fun

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
+1-multi_languages.js
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
