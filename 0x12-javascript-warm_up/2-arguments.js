18ab545aa3f43239337f92b2718e2f12d2188409
okotoazachristian <okotoazachristian@gmail.com>
2024-03-11 14:51:20 -0700

    task done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+1-multi_languages.js
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
