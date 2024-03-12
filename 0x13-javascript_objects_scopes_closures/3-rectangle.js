5a130cf9d1ebc9cb5ec39923f23ea5d30673a0dc
okotoazachristian <okotoazachristian@gmail.com>
2024-03-12 11:45:02 -0700

    alx task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,6 +6,16 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
 }
 
 module.exports = Rectangle;
